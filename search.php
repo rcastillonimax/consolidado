5fd06ee5dacb078e1c9ab7f8469121414a24734f
rcastillonimax <rcastillo@nimax.com.mx>
2015-01-26 18:29:58 -0600

    Primer Commit
    Empezamos a usar GitHub, porque eliminamos todo el trabajo ya hecho

---
+++ search.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Expires" content="Fri, Jan 01 1900 00:00:00 GMT">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Cache-Control" content="no-cache">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="Lang" content="en">
+        <meta name="author" content="">
+        <meta http-equiv="Reply-to" content="@.com">
+        <meta name="generator" content="PhpED 8.0">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="creation-date" content="09/06/2012">
+        <meta name="revisit-after" content="15 days">
+        <title>Untitled</title>
+        <link rel="stylesheet" type="text/css" href="my.css">
+    </head>
+    <body>
+        <div id="header-container">
+            <header class="wrapper clearfix">
+                <h1 id="title">CONSOLIDADO</h1>
+ <!-- Lista para MENU
+               <nav>
+                    <ul>
+                        <li><a href="#">nav ul li a</a></li>
+                        <li><a href="#">nav ul li a</a></li>
+                        <li><a href="#">nav ul li a</a></li>
+                    </ul>
+                </nav>              -->
+            </header>
+        </div>
+
+        <div id="main-container">
+            <div id="main" class="wrapper clearfix">
+
+                <article>
+                    <header>
+                       
+                    </header>
+                </article>
+
+                <aside>
+                </aside>
+
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+
+        <div id="footer-container">
+            <footer class="wrapper">
+            <!--<h3>footer</h3>-->
+                
+            </footer>
+        </div>
+    </body>
+</html>
