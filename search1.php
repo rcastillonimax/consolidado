7bc65610aea69bc316db4647ae29fc6a417a4155
rcastillonimax <rcastillo@nimax.com.mx>
2015-01-28 13:57:58 -0600

    Cambiamos forma de mostrar la tabla
    guardamos todos los datos en una variable y luego imprimimos, antes
    imprimiamos los datos en cuanto se obtenian..
    Agregamos lineas para mostrar una mejor tabla con Jquery y Table sorter

--- search1.php
+++ search1.php
@@ -15,6 +15,11 @@
         <meta name="revisit-after" content="15 days">
         <title>Untitled</title>
         <link rel="stylesheet" type="text/css" href="my.css">
+        <!--    Jquery para ordenacion y formato de tablas-->
+        <script type="text/javascript" src="../jquery/tablesorter/jquery-latest.js"></script> 
+        <script type="text/javascript" src="../jquery/tablesorter/jquery.tablesorter.js"></script> 
+
+
     </head>
     <body>
 
@@ -30,37 +35,41 @@
             $searchby=str_replace(" ","_",$searchby);
             $txtsearchby=$_REQUEST['txtsearchby'];
             //echo "-$txtsearchby-<br>";
-            
+
             //$query="SELECT * FROM $table WHERE '".$searchby."' = \`".$txtsearchby."\`";
             //SELECT * FROM $table '$txtsearchby' YA ESTA BIEN SOLO FALTA EL WHERE
             $query="SELECT * FROM $table WHERE `$searchby` = '$txtsearchby'";
             echo "-$query-<br>";
+            $table="";
+            //Si se ejecuta la consulta
             if($result=mysqli_query ($link,$query))
             {
-                $fields_num=mysqli_field_count($link);
-                echo"<h1>Table:";
-                echo "<table border ='1'><tr>";
+                echo $fields_num=mysqli_field_count($link);
+                if($fields_num>0)
+                { 
+                    $table=$table+"<h1>Table:</h1>";
+                    $table=$table."<table border ='1'><tr>";
 
-                //Imprimimos headers
-                for($i=0;$i<$fields_num;$i++)
-                {
-                    $field=mysqli_fetch_field($result);
-                    echo"<td>{$field->name}</td>";
-                }
+                    // headers
+                    for($i=0;$i<$fields_num;$i++)
+                    {
+                        $field=mysqli_fetch_field($result);
+                        $table=$table."<td>{$field->name}</td>";
+                    }
+                    $table=$table."</tr>";
 
-                echo "</tr>\n";
-
-                //Imprimimos tabla
-
-                while($row = mysqli_fetch_row($result))
-                {
-                    echo "<tr>";
-                    // $row is array... foreach( .. ) puts every element
-                    // of $row to $cell variable
-                    foreach($row as $cell)
-                        echo "<td>$cell</td>";
-                    echo "</tr>\n";
-                }
+                    // tabla
+                    while($row = mysqli_fetch_row($result))
+                    {
+                        $table=$table."<tr>";
+                        // $row is array... foreach( .. ) puts every element
+                        // of $row to $cell variable
+                        foreach($row as $cell)
+                            $table=$table."<td>$cell</td>";
+                        $table=$table."</tr>\n";
+                    }
+                    echo "imprimimos tabla: ".$table."sd";
+                } else echo "Sin resultado";
 
             } else echo "Error en consulta:<b>".mysqli_error($link);
 
