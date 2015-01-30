cad3508300a47ab4dd6506b98f9a4d584e5b5623
rcastillonimax <rcastillo@nimax.com.mx>
2015-01-30 14:48:54 -0600

    Corregimos estructura Tabla
    Se estaba definiendo mal la tabla

--- search1.php
+++ search1.php
@@ -5,60 +5,36 @@
         <meta http-equiv="Lang" content="en">
         <meta name="Raymundo Castillo" content="">
         <meta name="keywords" content="">
-
         <title>Consolidado - NIMAX</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
 
         <!--        <link rel="stylesheet" type="text/css" href="my.css">  -->
 
         <!--    Jquery para ordenacion y formato de tablas, si se descarga Jquery no es necesario las primeras 2 lineas-->
         <!--        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>-->
         <!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>-->
+        <!--        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">-->
+        <!--        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>-->
+ <!--       <link rel="stylesheet" type="text/css" href=" http://cdn.datatables.net/1.10.4/css/jquery.dataTables.css">
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="http://cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>        
+        <script>
+            $(document).ready(function() {
+                $('#example').DataTable();
+            } );
+        </script>            
+-->
+
+
 
         <!--        Directorio donde estan los SCRIPT-->
-        <script type="text/javascript" src="../jquery/tablesorter/jquery-latest.js"></script> 
-        <script type="text/javascript" src="../jquery/tablesorter/__jquery.tablesorter.js"></script> 
-         
 
-        <link rel="stylesheet" href="./example_files/jq.css" type="text/css" media="print, projection, screen">
-        <link rel="stylesheet" href="./example_files/style.css" type="text/css" id="" media="print, projection, screen">
-        <script type="text/javascript" src="./example_files/jquery-latest.js"></script><style type="text/css"></style>
-
-        <script type="text/javascript" src="./example_files/__jquery.tablesorter.js"></script>
-        <script type="text/javascript" src="./example_files/jquery.tablesorter.pager.js"></script>  
-        <script type="text/javascript" src="./example_files/chili-1.8b.js"></script>
-        <script type="text/javascript" src="./example_files/docs.js"></script>
-        <script type="text/javascript" src="./example_files/examples.js"></script>
 
 
         <!--        Script Prueba Jquery para ver si se esta ejecutando, ya probamos y si ejecuta-->
-        <script>
-            $(document).ready(function(){
-                $("#btn1").click(function(){
-                    alert("Text: " + $("#test").text());
-                });
-                $("#btn2").click(function(){
-                    alert("HTML: " + $("#test").html());
-                });
-
-                /*                $("#tablita").tablesorter({ 
-                headers: { 
-                0: {sorter: false}, 
-                1: {sorter: false} 
-                } 
-                }); 
-                */
-                /*                $('.tablesorter').tablesorter({ 
-                headers: { 
-                0: { sorter: false}, 
-                1: {sorter: false} 
-                } 
-                });  */
-
-
-                $("#tablita").tablesorter(); 
-
-            });
-        </script> 
 
     </head>
 
@@ -83,7 +59,9 @@
 
             //Ejecutamos Query
             //$query="SELECT * FROM $table WHERE '".$searchby."' = \`".$txtsearchby."\`";
+            //$query="SELECT * FROM $table";
             $query="SELECT * FROM $table WHERE `$searchby` = '$txtsearchby'";
+            
             echo "-$query-<br>";
 
             //VAriable que alojara el codigo HTML de la TABLA
@@ -98,7 +76,8 @@
                     $fields_num=mysqli_field_count($link);
                     $table=$table."<h1>Table: Se encontraron ".mysqli_num_rows($result)." registro(s)</h1>\n";
                     //comienza cabecera de la tabla
-                    $table=$table."<table name='tablita' id='tablita' class='tablesorter' border='1'>\n<thead>\n<tr>\n";
+                    /*                            $table=$table."<table data-role='table' data-mode='columntoggle' class='ui-responsive' id='myTable'>\n<thead>\n<tr>\n";*/
+                    $table=$table."<table class='display' id='example' cellspacing='0' width='100%'>\n\t<thead>\n\t\t<tr>\n";
 
                     // obtiene headers
                     //Revisar, probablemente se pueda cambiar por un while***
@@ -106,22 +85,23 @@
                     {
                         $field=mysqli_fetch_field($result);
                         $header=str_replace("_"," ",$field->name);
-                        $table=$table."<th class='header'>{$header}\n</th>\n";
+                        $table=$table."\t\t\t<th class='header'>\n{$header}\n\t\t\t</th>\n\n";
                     }
-                    $table=$table."</tr>\n</thead>\n";
+                    $table=$table."\t\t</tr>\n\t</thead>\n";
 
+                    $table=$table."\n\t<tbody>";
                     // Filas de la tabla
                     while($row = mysqli_fetch_row($result))
                     {
-                        $table=$table."<tbody>\n<tr>\n";
+                        $table=$table."\n\t\t<tr>\n";
                         // $row is array... foreach( .. ) puts every element
                         // of $row to $cell variable
                         foreach($row as $cell)
-                            $table=$table."<td>\n$cell\n</td>\n";
-                        $table=$table."</tr>\n";
+                            $table=$table."\n\t\t\t<td>\n$cell\n\t\t\t</td>\n";
+                        $table=$table."\t\t</tr>\n";
                     }
                     //Terminamos Tabla
-                    $table=$table."</tbody>";
+                    $table=$table."\n\t</tbody>";
                     //Imprimos Tabla
                     echo "$table";
                 } else echo "<H1><br>Sin resultados</H1>";
@@ -129,10 +109,6 @@
             } else echo "Error en consulta:<b>".mysqli_error($link);
 
         ?>
-
-        <p id="test">This is some <b>bold</b> text in a paragraph.</p>
-        <button id="btn1">Show Text</button>
-        <button id="btn2">Show HTML</button>
 
     </body>
 </html>
