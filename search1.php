42a116dd5add302343004f68ab7a7a3c91e6aaf9
rcastillonimax <rcastillo@nimax.com.mx>
2015-01-30 13:02:17 -0600

    Cambiaremos Script de Jquery
    Cambiaremos Script de Jquery para mostrar y acomodar la informacion!

--- search1.php
+++ search1.php
@@ -17,6 +17,17 @@
         <!--        Directorio donde estan los SCRIPT-->
         <script type="text/javascript" src="../jquery/tablesorter/jquery-latest.js"></script> 
         <script type="text/javascript" src="../jquery/tablesorter/__jquery.tablesorter.js"></script> 
+         
+
+        <link rel="stylesheet" href="./example_files/jq.css" type="text/css" media="print, projection, screen">
+        <link rel="stylesheet" href="./example_files/style.css" type="text/css" id="" media="print, projection, screen">
+        <script type="text/javascript" src="./example_files/jquery-latest.js"></script><style type="text/css"></style>
+
+        <script type="text/javascript" src="./example_files/__jquery.tablesorter.js"></script>
+        <script type="text/javascript" src="./example_files/jquery.tablesorter.pager.js"></script>  
+        <script type="text/javascript" src="./example_files/chili-1.8b.js"></script>
+        <script type="text/javascript" src="./example_files/docs.js"></script>
+        <script type="text/javascript" src="./example_files/examples.js"></script>
 
 
         <!--        Script Prueba Jquery para ver si se esta ejecutando, ya probamos y si ejecuta-->
@@ -28,6 +39,24 @@
                 $("#btn2").click(function(){
                     alert("HTML: " + $("#test").html());
                 });
+
+                /*                $("#tablita").tablesorter({ 
+                headers: { 
+                0: {sorter: false}, 
+                1: {sorter: false} 
+                } 
+                }); 
+                */
+                /*                $('.tablesorter').tablesorter({ 
+                headers: { 
+                0: { sorter: false}, 
+                1: {sorter: false} 
+                } 
+                });  */
+
+
+                $("#tablita").tablesorter(); 
+
             });
         </script> 
 
@@ -37,7 +66,7 @@
 
         <?php
             //Hara una consulta a una DB en MySQL y con los datos obtenidos generara una tabla
-        
+
             include('../php/connect.php');
             $namedb="consolidado_schema";
             $table="reportes2014";
@@ -45,18 +74,18 @@
             //recuperamos criterio busqueda
             $searchby=$_REQUEST['searchby'];
             //echo "-$searchby-<br>";
-            
+
             //cambiar " " espacios por "_"
             $searchby=str_replace(" ","_",$searchby);
             //Convertimos a Mayusculas
             $txtsearchby=strtoupper($_REQUEST['txtsearchby']);
             //echo "-$txtsearchby-<br>";
-            
+
             //Ejecutamos Query
             //$query="SELECT * FROM $table WHERE '".$searchby."' = \`".$txtsearchby."\`";
             $query="SELECT * FROM $table WHERE `$searchby` = '$txtsearchby'";
             echo "-$query-<br>";
-            
+
             //VAriable que alojara el codigo HTML de la TABLA
             $table="";
             //Contruir Tabla
@@ -67,9 +96,9 @@
                 if(mysqli_num_rows($result)>0)
                 { 
                     $fields_num=mysqli_field_count($link);
-                    $table=$table."<h1>Table: Se encontraron ".mysqli_num_rows($result)." registro(s)</h1>";
+                    $table=$table."<h1>Table: Se encontraron ".mysqli_num_rows($result)." registro(s)</h1>\n";
                     //comienza cabecera de la tabla
-                    $table=$table."<table class='tablesorter' border='1'><thead><tr>";
+                    $table=$table."<table name='tablita' id='tablita' class='tablesorter' border='1'>\n<thead>\n<tr>\n";
 
                     // obtiene headers
                     //Revisar, probablemente se pueda cambiar por un while***
@@ -77,18 +106,18 @@
                     {
                         $field=mysqli_fetch_field($result);
                         $header=str_replace("_"," ",$field->name);
-                        $table=$table."<th class='header'>{$header}</th>";
+                        $table=$table."<th class='header'>{$header}\n</th>\n";
                     }
-                    $table=$table."</tr></thead>";
+                    $table=$table."</tr>\n</thead>\n";
 
                     // Filas de la tabla
                     while($row = mysqli_fetch_row($result))
                     {
-                        $table=$table."<tbody><tr>";
+                        $table=$table."<tbody>\n<tr>\n";
                         // $row is array... foreach( .. ) puts every element
                         // of $row to $cell variable
                         foreach($row as $cell)
-                            $table=$table."<td>$cell</td>";
+                            $table=$table."<td>\n$cell\n</td>\n";
                         $table=$table."</tr>\n";
                     }
                     //Terminamos Tabla
@@ -104,22 +133,6 @@
         <p id="test">This is some <b>bold</b> text in a paragraph.</p>
         <button id="btn1">Show Text</button>
         <button id="btn2">Show HTML</button>
-
-
-        <!--        <script >
-        $(document).ready(function() 
-        { 
-        $("#myTable").tablesorter(); 
-        } 
-        );
-
-
-        </script> -->  
-        <script type="text/javascript" id="js">
-           $('table').tablesorter({ headers: { 0: { sorter: false}, 1: {sorter: false} } });
-        </script>
-
-
 
     </body>
 </html>
