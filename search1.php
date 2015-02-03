05c0eee3b59b14df9175197ca570d273c981416b
rcastillonimax <rcastillo@nimax.com.mx>
2015-02-03 13:26:12 -0600

    Formato
    Desactivamos que se reemplace _ por " " para que los headers queden en
    una sola columna
    Se agregaron espacio para que disminuya el largo de las columnas con
    demasiado TEXTO falla real 27 y observaciones 35

--- search1.php
+++ search1.php
@@ -16,7 +16,7 @@
 
         <!--    Jquery para ordenacion y formato de tablas, si se descarga Jquery no es necesario las primeras 2 lineas-->
         <!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>-->
-        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>-->
+        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
         <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
         <script src="http://cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
 
@@ -29,29 +29,28 @@
         -->
 
 
-<script type="text/javascript" class="init">
+        <script type="text/javascript" class="init">
 
 
-$(document).ready(function() {
-    var table = $('#example').DataTable( {
-        /*"scrollY": "200px",*/
-        "paging": false
-    } );
+            $(document).ready(function() {
+                var table = $('#example').DataTable( {
+                    /*"scrollY": "200px",*/
+                    "paging": true
+                } );
 
-    $('a.toggle-vis').on( 'click', function (e) {
-        e.preventDefault();
+                $('a.toggle-vis').on( 'click', function (e) {
+                    e.preventDefault();
 
-        // Get the column API object
-        var column = table.column( $(this).attr('data-column') );
+                    // Get the column API object
+                    var column = table.column( $(this).attr('data-column') );
 
-        // Toggle the visibility
-        column.visible( ! column.visible() );
-    } );
-} );
+                    // Toggle the visibility
+                    column.visible( ! column.visible() );
+                } );
+            } );
 
 
-    </script>      
-
+        </script>    
 
 
 
@@ -62,97 +61,100 @@ $(document).ready(function() {
     </head>
 
     <body>
-    <div>
+        <div>
 
-            <?php
-                //Hara una consulta a una DB en MySQL y con los datos obtenidos generara una tabla
+        <?php
+            //Hara una consulta a una DB en MySQL y con los datos obtenidos generara una tabla
 
-                include('../php/connect.php');
-                $namedb="consolidado_schema";
-                $table="reportes2014";
-                mysqli_select_db($link,$namedb);
-                //recuperamos criterio busqueda
-                $searchby=$_REQUEST['searchby'];
-                //echo "-$searchby-<br>";
+            include('../php/connect.php');
+            $namedb="consolidado_schema";
+            $table="reportes2014";
+            mysqli_select_db($link,$namedb);
+            //recuperamos criterio busqueda
+            $searchby=$_REQUEST['searchby'];
+            //echo "-$searchby-<br>";
 
-                //cambiar " " espacios por "_"
-                $searchby=str_replace(" ","_",$searchby);
-                //Convertimos a Mayusculas
-                $txtsearchby=strtoupper($_REQUEST['txtsearchby']);
-                //echo "-$txtsearchby-<br>";
+            //cambiar " " espacios por "_"
+            $searchby=str_replace(" ","_",$searchby);
+            //Convertimos a Mayusculas
+            $txtsearchby=strtoupper($_REQUEST['txtsearchby']);
+            //echo "-$txtsearchby-<br>";
 
-                //Ejecutamos Query
-                //$query="SELECT * FROM $table WHERE '".$searchby."' = \`".$txtsearchby."\`";
-                //$query="SELECT * FROM $table";
-                $query="SELECT * FROM $table WHERE `$searchby` = '$txtsearchby'";
+            //Ejecutamos Query
+            //$query="SELECT * FROM $table WHERE '".$searchby."' = \`".$txtsearchby."\`";
+            //$query="SELECT * FROM $table";
+            $query="SELECT * FROM $table WHERE `$searchby` = '$txtsearchby'";
 
-                //                echo "-$query-<br>";
+            //                echo "-$query-<br>";
 
-                //VAriable que alojara el codigo HTML de la TABLA
-                $table=$hideColumn="";
-                //Contruir Tabla
-                //Si se ejecuta la consulta
-                if($result=mysqli_query ($link,$query))
-                {
-                    //Si la consulta no esta vacia
-                    if(mysqli_num_rows($result)>0)
-                    { 
-                        
-                        $fields_num=mysqli_field_count($link);
-                        $hideColumn="Elije columnas a ocultar\n<br>";
-                        $table=$table."<h1>Table: Se encontraron ".mysqli_num_rows($result)." registro(s)</h1>\n";
-                        //comienza cabecera de la tabla
+            //VAriable que alojara el codigo HTML de la TABLA
+            $table=$hideColumn="";
+            //Contruir Tabla
+            //Si se ejecuta la consulta
+            if($result=mysqli_query ($link,$query))
+            {
+                //Si la consulta no esta vacia
+                if(mysqli_num_rows($result)>0)
+                { 
 
-                        /*                        $table=$table."<table data-role='table' data-mode='columntoggle' class='ui-responsive' class='display' id='example' cellspacing='0' width='100%'>\n\t<thead>\n\t\t<tr>\n";
-                        */
-                        $table=$table."<table class='display' id='example' cellspacing='0' width='100%' border='1'>\n\t<thead>\n\t\t<tr>\n";
+                    $fields_num=mysqli_field_count($link);
+                    $hideColumn="Elije columnas a ocultar\n<br>";
+                    $table=$table."<h1>Table: Se encontraron ".mysqli_num_rows($result)." registro(s)</h1>\n";
+                    //comienza cabecera de la tabla
 
-                        
-                         for($i=0;$i<$fields_num;$i++)
-                        {
-                            
-                            $table=$table."\t\t\t<td>\n$i\n\t\t\t</td>\n\n";
-                        }
-                        $table=$table."\t\t</tr>\n\t\n\t\t<tr>";
-                        
-                        
-                        
-                        
-                        // obtiene headers
-                        //Revisar, probablemente se pueda cambiar por un while***
-                        for($i=0;$i<$fields_num;$i++)
-                        {
-                            $field=mysqli_fetch_field($result);
-                            if($i!=27 or $i!=35)
-                            $header=str_replace("_"," ",$field->name);
-                            /*$table=$table."\t\t\t<th class='header'>\n{$header}\n\t\t\t</th>\n\n";*/
-                            if($i==27 or $i==35) {
-                                $espacios="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-                                $table=$table."\t\t\t<th class='header'>\n$espacios{$header}$espacios\n\t\t\t</th>\n\n";
-                            } else $table=$table."\t\t\t<th class='header'>\n{$header}\n\t\t\t</th>\n\n";
-                            
-                            
-                            $hideColumn=$hideColumn."<a class='toggle-vis' data-column='$i'>{$i}</a> - \n";
-                        }
-                        $table=$table."\t\t</tr>\n\t</thead>\n";
-                        $table=$table."\n\t<tbody>";
-                        // Filas de la tabla
-                        while($row = mysqli_fetch_row($result))
-                        {
-                            $table=$table."\n\t\t<tr>\n";
-                            // $row is array... foreach( .. ) puts every element
-                            // of $row to $cell variable
-                            foreach($row as $cell)
-                                $table=$table."\n\t\t\t<td>\n$cell\n\t\t\t</td>\n";
-                            $table=$table."\t\t</tr>\n";
-                        }
-                        //Terminamos Tabla
-                        $table=$table."\n\t</tbody>";
-                        //Imprimos Tabla
-                        echo $hideColumn."\n"."$table";
-                    } else echo "<H1><br>Sin resultados</H1>";
-                } else echo "Error en consulta:<b>".mysqli_error($link);
-            ?>
+                    /*                        $table=$table."<table data-role='table' data-mode='columntoggle' class='ui-responsive' class='display' id='example' cellspacing='0' width='100%'>\n\t<thead>\n\t\t<tr>\n";
+                    */
+                    $table=$table."<table class='display' id='example' cellspacing='0' width='100%' border='1'>\n\t<thead>\n\t\t<tr>\n";
+
+
+                    for($i=0;$i<$fields_num;$i++)
+                    {
+
+                        $table=$table."\t\t\t<td>\n$i\n\t\t\t</td>\n\n";
+                    }
+                    $table=$table."\t\t</tr>\n\t\n\t\t<tr>";
+
+
+
+
+                    // obtiene headers
+                    //Revisar, probablemente se pueda cambiar por un while***
+                    for($i=0;$i<$fields_num;$i++)
+                    {
+                        $field=mysqli_fetch_field($result);
+                        //Reemplazamos _ por espacio, 
+                        /*if($i!=27 or $i!=35)
+                        $header=str_replace("_"," ",$field->name);*/
+                        $header=$field->name;
+
+                        /*$table=$table."\t\t\t<th class='header'>\n{$header}\n\t\t\t</th>\n\n";*/
+                        if($i==27 or $i==35) {
+                            $espacios="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+                            $table=$table."\t\t\t<th class='header'>\n$espacios{$header}$espacios\n\t\t\t</th>\n\n";
+                        } else $table=$table."\t\t\t<th class='header'>\n{$header}\n\t\t\t</th>\n\n";
+
+
+                        $hideColumn=$hideColumn."<a class='toggle-vis' data-column='$i'>{$i}</a> - \n";
+                    }
+                    $table=$table."\t\t</tr>\n\t</thead>\n";
+                    $table=$table."\n\t<tbody>";
+                    // Filas de la tabla
+                    while($row = mysqli_fetch_row($result))
+                    {
+                        $table=$table."\n\t\t<tr>\n";
+                        // $row is array... foreach( .. ) puts every element
+                        // of $row to $cell variable
+                        foreach($row as $cell)
+                            $table=$table."\n\t\t\t<td>\n$cell\n\t\t\t</td>\n";
+                        $table=$table."\t\t</tr>\n";
+                    }
+                    //Terminamos Tabla
+                    $table=$table."\n\t</tbody>";
+                    //Imprimos Tabla
+                    echo $hideColumn."\n"."$table";
+                } else echo "<H1><br>Sin resultados</H1>";
+            } else echo "Error en consulta:<b>".mysqli_error($link);
+        ?>
 
 
     </body>
